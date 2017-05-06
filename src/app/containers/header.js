4ccd2062460712cedfba29057f3fee9f5f5344f5
andreelorenz <andreelorenz@gmail.com>
2017-05-06 20:33:15 -0300

    Favorite

--- src/app/containers/header.js
+++ src/app/containers/header.js
@@ -1,23 +1,33 @@
 import { STATES_CITIES } from './../constants/states-cities';
 
 class HeaderController {
-  constructor(forecastService) {
+  constructor($localStorage, forecastService) {
+    this.$localStorage = $localStorage;
     this.forecastService = forecastService;
     this.getStates();
   }
 
   getForecast() {
-    const a = undefined;
-    return this.forecastService.getForecast(this.stateSelected.initials, this.citySelected, a);
+    return this.forecastService.getForecast(this.stateSelected.initials, this.citySelected);
+  }
+
+  setFavorite() {
+    this.forecastService.setFavorite(this.stateSelected, this.citySelected);
   }
 
   getStates() {
     this.states = STATES_CITIES.map(state => ({ name: state.nome, initials: state.sigla }));
+    this.favorite = this.forecastService.getFavorite();
+    if (this.favorite) {
+      this.stateSelected = this.states.find(state => state.initials === this.favorite.state.initials);
+      this.loadCities(true);
+    }
   }
 
-  loadCities() {
+  loadCities(favorite) {
     if (this.stateSelected) this.cities = STATES_CITIES.find(state => state.sigla === this.stateSelected.initials).cidades;
     else this.cities = [];
+    if (favorite) this.citySelected = this.favorite.city;
   }
 }
 
